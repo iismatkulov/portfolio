2da2c5f011a761a885ba45413af0c892c551e414
iismatkulov <ismatkulovislom@gmail.com>
2021-12-19 20:22:40 +0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -74,7 +74,7 @@ function(o) {
                     m = $('.modal-overlay, .modal'),
                     i = r.data("success"),
                     d = e.data("email");
-                return r.empty(), "" === t || "" === n || "" === a || "" === d || "" === s ? o(".empty_notice").slideDown(500).delay(2e3).slideUp(500) : o.post(`https://api.telegram.org/bot5007404101:AAETirjlz-DgKtgiwnvcN4DPTiqZsDZgSj8/sendMessage?chat_id=52238804&text=Сообщение от: ${t}<pre>______________________________</pre> Номер телефона: ${s} <pre>______________________________</pre> Эл.почта: ${n} <pre>______________________________</pre> Сообщение: ${a}&parse_mode=HTML`, {
+                return r.empty(), "" === t || "" === n || "" === a || "" === d || "" === s ? o(".empty_notice").slideDown(500).delay(2e3).slideUp(500) : o.post(`https://api.telegram.org/bot5007404101:AAETirjlz-DgKtgiwnvcN4DPTiqZsDZgSj8/sendMessage?chat_id=52238804&text=<b>Сообщение от:</b> ${t}<pre>______________________________</pre> <b>Номер телефона:</b> ${s} <pre>______________________________</pre> <b>Эл.почта:</b> ${n} <pre>______________________________</pre> <b>Сообщение:</b> ${a}&parse_mode=HTML`, {
                     ajax_name: t,
                     ajax_email: n,
                     ajax_emailto: d,
